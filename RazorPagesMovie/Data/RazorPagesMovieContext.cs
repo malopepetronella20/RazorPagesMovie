8658cfa8d441d99533a9916916bb3e00e3f7f9ef
malopepetronella20 <malopepetronella20gmail.com>
2025-11-20 15:38:23 +0200

    updated RazorPagesMovieContext.cs, css

--- RazorPagesMovie/Data/RazorPagesMovieContext.cs
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Migrations;
 using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Data
 {
-    public class RazorPagesMovieContext : DbContext
+    public class RazorPagesMovieContext : IdentityDbContext
     {
         public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
             : base(options)
         {
         }
 
-        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Rating> Rating { get; set; }
     }
 }
